4426b480a401ac82dcd6337baf8bacdedafa2990
Aroshakalpana <89211301+Aroshakalpana2468@users.noreply.github.com>
2022-02-18 22:53:22 +0530

    Update script.py

--- script.py
+++ script.py
@@ -14,11 +14,11 @@ For more click help....</b>"""
 <code>🌀 Your Edited Image will be Uploaded </code>
 
 
-© @trtechguide"""
+© @aastream"""
     ABOUT_MSG = """⭕️<b>My Name : Image Editor</b>
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TR-TECH-GUIDE/Image-Editor'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='pissuda ewa denna baa😂😂'>Click Here</a>"""
